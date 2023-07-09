701df4442d32182e5a1e4a76394a98c0e0351851
Ashutosh Joshi <ashutoshjosheee@gmail.com>
2023-07-10 00:00:10 +0530

    second modification

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+
+const {v4 : uuidv4} = require('uuid')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-var
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended : false}))
+
+
 var tad=[{id:1,title:"milk",description:"at 9am"
 },{id:2,title:"Food",description:"at 11am"}]
 // 1.GET /todos - Retrieve all todo items
@@ -38,12 +44,64 @@ app.get('/todos/:idd', (req, res) => {
 //   description: "I should buy groceries" }
 
 app.post('/todos', (req, res) => {
- var obj=req.params.
- 
-    res.status(201).json();
+ //var body=req.body
+
+ console.log(req.body.title);
+ const userId = uuidv4()
+ let obj=
+  {id:userId,title:req.body.title,description:req.body.description};
+ tad.push(obj)
+    res.status(201).send(userId);
   })
 
+// 4. PUT /todos/:id - Update an existing todo item by ID
+//     Description: Updates an existing todo item identified by its ID.
+//     Request Body: JSON object representing the updated todo item.
+//     Response: 200 OK if the todo item was found and updated, or 404 Not Found if not found.
+//     Example: PUT http://localhost:3000/todos/123
+//     Request Body: { "title": "Buy groceries", "completed": true }
 
+app.put('/todos/:id',(req,res)=>{
+//console.log("hello world");
+let idd=req.params.id;
+let index=tad.findIndex(t=>t.id==idd)
+console.log(req.body.title);
+if(idd==-1){
+  console.log("id doesnt exists")
+  res.status(404).send("404 Not Found")
+}
+else{
+ let obj=
+  {
+    id:idd,
+    title:req.body.title,
+    description:req.body.description
+  };
+ tad[index]=obj;
+ res.status(200).json(obj);
+  
+}
+
+})
+// 5. DELETE /todos/:id - Delete a todo item by ID
+//     Description: Deletes a todo item identified by its ID.
+//     Response: 200 OK if the todo item was found and deleted, or 404 Not Found if not found.
+//     Example: DELETE http://localhost:3000/todos/123
+
+app.delete('/todos/:id', (req, res) => {
+  
+  let index=tad.findIndex(t=>t.id==req.params.id);
+  if(index==-1){
+    res.status(404).send("404 not found");
+  }
+  else{
+    let arr1=tad.slice(index+1);
+    let arr2=tad.slice(0,index)
+    tad=arr1.concat(arr2);
+res.status(200).json(tad);
+  }
+   
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
